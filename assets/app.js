b7c69181ab38eb61f3b831584d7e98b0b259956b
unknown <pp_nancy@hotmail.com>
2019-05-16 23:19:22 -0400

    saved

--- assets/app.js
+++ assets/app.js
@@ -1,99 +1,29 @@
-var athletesArray = ["Kobe Bryant", "Michael Jordan", "Larry Bird", "Steve Young", "Troy Aikman"];
+var cartoons = ["Mickey mouse ", "Donald Duck ", "Ariel ", "The Lion King "];
+    function renderButtons() {
+      $("#cartoons-view").empty();
 
-$(document).ready(function() {
-    for (var i = 0; i < athletesArray.length; i++) {
-        $("#athlete-buttons").append("<button type='button' onclick='searchGif(\"" + athletesArray[i] + "\")' class='btn btn-primary' value=' " + athletesArray[i] + "'> " + athletesArray[i] + " </button>");
-    }
-});
-
-function athleteButtonClicked() {
-    var userInput = $('#athlete-input').val();
-    searchGif(userInput);
-}
-
-function submitButtonClicked() {
-    var userInput = $('#athlete-input').val();
-
-    if (userInput) {
-        $('#athlete-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
-    }
-}
-
-function searchGif(gifName) {
-    $.ajax({
-            url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=dc6zaTOxFJmzC',
-            type: 'GET',
-        })
-        .done(function(response) {
-            displayGif(response);
-        })
-}
-
-function displayGif(response) {
-    $('#athletes').empty();
-    for (var i = 0; i < response.data.length; i++) {
-        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
-        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
-            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
-            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
-
-        image = '<div class="col-md-4">' + image + "</div>";
-        $('#athletes').append(image);
-    }
-
-    $('.movImage').on('click', function() {
-        var state = $(this).attr('data-state');
-        if (state == 'still') {
-            $(this).attr('src', $(this).attr("data-animate"));
-            $(this).attr('data-state', 'animate');
-        } else {
-            $(this).attr('src', $(this).attr("data-still"));
-            $(this).attr('data-state', 'still');
-        }
-
-    });
-
-    var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
-
-      // Function for displaying movie data
-      function renderButtons() {
-
-        // Deleting the movie buttons prior to adding new movie buttons
-        // (this is necessary otherwise we will have repeat buttons)
-        $("#movies-view").empty();
-
-        // Looping through the array of movies
-        for (var i = 0; i < movies.length; i++) {
-
-          // Then dynamicaly generating buttons for each movie in the array.
-          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-          var a = $("<button>");
+      for (var i = 0; i < cartoons.length; i++) {
+        var a = $("<button>");
           // Adding a class
-          a.addClass("movie");
-          // Adding a data-attribute with a value of the movie at index i
-          a.attr("data-name", movies[i]);
-          // Providing the button's text with a value of the movie at index i
-          a.text(movies[i]);
+          a.addClass("cartoon");
+          // Adding a data-attribute with a value of the cartoon at index i
+          a.attr("data-name", cartoons[i]);
+          // Providing the button's text 
+          a.text(cartoons[i]);
           // Adding the button to the HTML
-          $("#movies-view").append(a);
+          $("#cartoons-view").append(a);
         }
       }
 
-      // This function handles events where one button is clicked
-      $("#add-movie").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
+      $("#add-cartoon").on("click", function(event) {
         event.preventDefault();
 
-        // This line will grab the text from the input box
-        var movie = $("#movie-input").val().trim();
-        // The movie from the textbox is then added to our array
-        movies.push(movie);
+var cartoon = $("#cartoon-input").val().trim();
 
-        // calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      });
+cartoons.push(cartoon);
+$("#cartoon-input").empty();
 
-      // Calling the renderButtons function at least once to display the initial list of movies
-      renderButtons();
-}
+renderButtons();
+});
+renderButtons();
+   
